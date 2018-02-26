2f489be073362a044fef759fda504b3783381e9c
git <rltgksek>
2018-02-26 22:46:11 +0900

    -- 작업내역
    1. 배포 스크립트 추가

--- frontend/src/lib/axiosAuth.js
+++ frontend/src/lib/axiosAuth.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 import { ACCESS_HEADER_TOKEN, ACCESS_TOKEN } from './constants';
 
-let axiosAuth = axios.create();
+let axiosAuth = axios.create({
+    header: {
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods' : '*'
+    }
+});
 axiosAuth.defaults.headers.common[ACCESS_HEADER_TOKEN] = localStorage.getItem(ACCESS_TOKEN);
 
 export default axiosAuth;
