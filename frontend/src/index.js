b484fa6675abaa82c230bec6bbaaa0dec66d4427
sunginHwang <gommpo111@gmail.com>
2018-02-28 11:02:43 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 if(isProd){
     axiosAuth.defaults.withCredentials = true;
+    axiosAuth.defaults.baseURL = 'http://api.mrh.kr';
 }
 
 const store = configureStore();
