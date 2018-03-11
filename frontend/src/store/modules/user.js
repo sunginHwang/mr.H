2fd5129a11d288bb3b79a04cc100e880a1312f5c
git <rltgksek>
2018-03-11 13:25:37 +0900

    -- 작업내역
    1. 회원가입 시 호출 API 이상 부분 변경

--- frontend/src/store/modules/user.js
+++ frontend/src/store/modules/user.js
@@ -2,7 +2,7 @@
  * Created by hwangseong-in on 2017. 10. 6..
  */
 import { createAction, handleActions } from 'redux-actions';
-import axios from 'axios';
+import axiosAuth from 'lib/axiosAuth';
 import { pender } from 'redux-pender';
 import { Map } from 'immutable';
 import { getErrorMsg } from 'lib/util';
@@ -13,7 +13,7 @@ const CHANGE_USER_INPUT_VALUE = 'user/CHANGE_USER_INPUT_VALUE';
 const SET_VALIDATE_ERROR_MESSAGE = 'user/SET_VALIDATE_ERROR_MESSAGE';
 const INITIAL_USER_INFO = 'user/INITIAL_USER_INFO';
 
-const apiUserRegister = (userId, password, userName, email) => axios.post(`/api/auth/register`,{userId : userId, userPassword : password, userName : userName, userEmail: email });
+const apiUserRegister = (userId, password, userName, email) => axiosAuth.post(`/api/auth/register`,{userId : userId, userPassword : password, userName : userName, userEmail: email });
 
 //액션 생성자
 export const registerUser = createAction(REGISTER_USER,apiUserRegister);
